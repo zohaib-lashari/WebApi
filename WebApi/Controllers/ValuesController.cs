574c73e14995fc28b2b76d56332f78e3791e5df1
zohaib <zohaiblashari781@gmail.com>
2024-10-02 15:44:30 +0500

    create first api

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> values()
+        {
+
+            return new List<Villa>
+            {
+                new Villa {Id = 1,Name = "Pool view" },
+                new Villa {Id = 2,Name = "Beach view"}
+
+            };
+        }
+    }
+}
